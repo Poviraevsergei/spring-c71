6ef7e1a6ba0b738e6f63ba136617813a560eeb58
Poviraevsergei <poviraevsergei@gmail.com>
2024-03-12 22:00:28 +0300

    controller findAll

---
+++ src/main/java/com/spring/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.spring.controller;
+
+import com.spring.model.User;
+import com.spring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller //MVC где есть страницы
+@RequestMapping("/user") //все методы этого контроллера начинаются с /user
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String getAllUsers(ModelMap modelMap) {
+        List<User> users = userService.getAllUsers();
+        modelMap.addAttribute("users",users);
+        return users.isEmpty() ? "empty" : "get_all_users";
+    }
+}
---
+++ src/main/webapp/WEB-INF/pages/empty.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Siarhey.Pavirayeu
+  Date: 3/12/2024
+  Time: 9:47 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Empty</title>
+</head>
+<body>
+We do not have result !
+</body>
+</html>
